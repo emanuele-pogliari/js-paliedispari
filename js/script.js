cdd2caf4791ed875ea8090cf5fd7236d7e4ed2ca
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-07 13:52:21 +0100

    remove matrix graphic

--- js/script.js
+++ js/script.js
@@ -1,81 +1,119 @@
-// Pari e Dispari
-// L’utente sceglie pari o dispari.
-// Inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Se l'utente ha indovinato l'esito (pari o dispari) della somma ha vinto, altrimenti ha perso.
+// // Initialising the canvas
+// var canvas = document.querySelector('canvas'),
+//     ctx = canvas.getContext('2d');
 
+// // Setting the width and height of the canvas
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
-// let sceltaUtente;
-// let numero;
-// let randomNumber;
-// sceltaUtente = prompt("Scegli pari o dispari");
+// // Setting up the letters
+// var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+// letters = letters.split('');
 
-// while (!(sceltaUtente === "pari" || sceltaUtente === "dispari")) {
-//     sceltaUtente = prompt("Scelta non valida: Scegli 'pari' o 'dispari'");
-// }
-// numero = Number(prompt("Inserisci un numero. Inserisci un numero da 1 a 5"));
+// // Setting up the columns
+// var fontSize = 10,
+//     columns = canvas.width / fontSize;
 
-// while (numero < 1 || numero > 5 || isNaN(numero)) {
-//     numero = Number(prompt("Scelta non valida. Inserisci un numero da 1 a 5"));
-// }
-
-// randomNumber = random();
-// let sum = 0;
-
-// sum = numero + randomNumber;
-// let esito = check(sum);
-
-// console.log("Numero Utente " + numero);
-// console.log("Scelta utente: " + sceltaUtente);
-// console.log("Numero casuale: " + randomNumber);
-// console.log("La somma dei numeri è " + sum);
-// console.log(esito);
-
-// if (sceltaUtente === esito) {
-//     console.log("Utente ha vinto");
-// } else {
-//     console.log("Computer ha vinto");
+// // Setting up the drops
+// var drops = [];
+// for (var i = 0; i < columns; i++) {
+//     drops[i] = 1;
 // }
 
-// function random() {
-//     return Math.floor(Math.random() * 5 + 1);
-// }
-
-// function check(somma) {
-
-//     if (somma % 2 === 0) {
-//         return "pari";
-//     } else {
-//         return "dispari"
+// // Setting up the draw function
+// function draw() {
+//     ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
+//     for (var i = 0; i < drops.length; i++) {
+//         var text = letters[Math.floor(Math.random() * letters.length)];
+//         ctx.fillStyle = '#0f0';
+//         ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+//         drops[i]++;
+//         if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+//             drops[i] = 0;
+//         }
 //     }
 // }
 
+// // Loop the animation
+// setInterval(draw, 33);
 
 
-// 
 
-// function created
-function trovaPalindromo(parola) {
-    // faccio un ciclo che sarà lungo la metà della parola inserita, se la prima lettera è diversa dall'ultima ritorna il valore il false altrimenti cicla fino al termine di tutti i cicli.
-    // Ritorna true alla fine.
-    for (let i = 0; i < Math.floor(parola.length / 2); i++) {
-        if (parola[i] !== parola[parola.length - (i + 1)]) {
-            return false;
+const buttonOoeElement = document.querySelector("#btn-start1");
+const buttonPalindromeElement = document.querySelector("#btn-start2");
+
+// avvio l'app al click del button
+buttonOoeElement.addEventListener("click", evenOrOdd);
+
+function evenOrOdd() {
+    let sceltaUtente;
+    let numero;
+    let randomNumber;
+    sceltaUtente = prompt("Scegli pari o dispari").toLowerCase;
+
+    // fino a quando l'utente non scegli pari o dispari chiedi all'utente di inserire la scelta
+    while (!(sceltaUtente === "pari" || sceltaUtente === "dispari")) {
+        sceltaUtente = prompt("Scelta non valida: Scegli 'pari' o 'dispari'");
+    }
+    numero = Number(prompt("Inserisci un numero. Inserisci un numero da 1 a 5"));
+    // fino a quando l'utente non inserisce un numero da 1 a 5 chiedi all'utente di riprovare
+    while (numero < 1 || numero > 5 || isNaN(numero)) {
+        numero = Number(prompt("Scelta non valida. Inserisci un numero da 1 a 5"));
+    }
+
+    // chiamo la funziona random per generare un numero casuale e lo salvo sulla variabile randomNumber
+    randomNumber = random();
+    let sum = 0;
+
+    //faccio la somma tra il numero scelto dall'utente ed il numero casuale
+    sum = numero + randomNumber;
+    // creo una variabile che controlla se la somma è pari o dispari e ritorna il valore
+    let esito = check(sum);
+
+
+    // controllo che verifica se ha vinto il computer o l'utente
+    if (sceltaUtente === esito) {
+        console.log("Utente ha vinto");
+    } else {
+        console.log("Computer ha vinto");
+    }
+
+    // prototipo di funzione per generare numeri casuali da 1 a 5
+    function random() {
+        return Math.floor(Math.random() * 5 + 1);
+    }
+    // prototipo di funzione per controllare la somma dei numeri e generare l'esito
+    function check(somma) {
+
+        if (somma % 2 === 0) {
+            return "pari";
+        } else {
+            return "dispari"
         }
     }
-    return true;
 }
 
-let word;
-word = prompt("Inserisci una parola");
-while (!isNaN(word)) {
-    word = prompt("Inserisci una parola");
-}
+// buttonPalindromeElement.addEventListener("click", findPalindrome);
 
-console.log(trovaPalindromo(word));
+// function findPalindrome() {
+//     // function created
+//     function trovaPalindromo(parola) {
+//         // faccio un ciclo che sarà lungo la metà della parola inserita, se la prima lettera è diversa dall'ultima ritorna il valore il false altrimenti cicla fino al termine di tutti i cicli.
+//         // Ritorna true alla fine.
+//         for (let i = 0; i < Math.floor(parola.length / 2); i++) {
+//             if (parola[i] !== parola[parola.length - (i + 1)]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     let word;
+//     word = prompt("Inserisci una parola");
+//     while (!isNaN(word)) {
+//         word = prompt("Inserisci una parola");
+//     }
 
-
-
+//     console.log(trovaPalindromo(word));
+// }
 
